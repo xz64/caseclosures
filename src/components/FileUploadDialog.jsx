1342b82927120dbf3e17c36ff92ea7b775b3eeae
S K <xz64@users.noreply.github.com>
2016-05-07 23:43:57 -0400

    add id to file upload input

--- src/components/FileUploadDialog.jsx
+++ src/components/FileUploadDialog.jsx
@@ -1,6 +1,6 @@
 export default (props) => (
   <div>
-    <input type="file" name="file" className="fileupload"/>
+    <input type="file" name="file" className="fileupload" id="fileupload"/>
     <button type="button" onClick={props.submit}>Submit</button>
   </div>
 );
